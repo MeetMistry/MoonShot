63c74ddd9d647980663b97d44cb90d4cf889a113
Meet Mistry <meet.m@simformsolutions.com>
2024-10-22 07:35:44 +0530

    Initial commit

---
+++ MoonShot/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MoonShot
+//
+//  Created by Meet Mistry on 22/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoonShotTests/MoonShotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotTests.swift
+//  MoonShotTests
+//
+//  Created by Meet Mistry on 22/10/24.
+//
+
+import Testing
+@testable import MoonShot
+
+struct MoonShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
